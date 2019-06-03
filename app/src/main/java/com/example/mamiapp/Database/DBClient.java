d3c13e18294bd72d1fbfabe84b4db4cef5639431
U.A. Yanık <ates.yanik@hva.nl>
2019-06-03 15:05:39 +0200

    some icon cleanup

--- app/src/main/java/com/example/mamiapp/Database/DBClient.java
+++ app/src/main/java/com/example/mamiapp/Database/DBClient.java
@@ -8,14 +8,13 @@ public class DBClient {
     private Context mCtx;
     private static DBClient mInstance;
 
-    //our app database object
+
     private ProductDB appDatabase;
 
     private DBClient(Context mCtx) {
         this.mCtx = mCtx;
 
         //creating the app database with Room database builder
-        //MyToDos is the name of the database
         appDatabase = Room.databaseBuilder(mCtx, ProductDB.class, "ProductsDB").build();
     }
 
