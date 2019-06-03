92f76dbfeaae627489c1e44324cdb74754b4e691
U.A. Yanık <ates.yanik@hva.nl>
2019-06-03 18:50:28 +0200

    close button implemented

--- app/src/main/java/com/example/mamiapp/ProductDetailsActivity.java
+++ app/src/main/java/com/example/mamiapp/ProductDetailsActivity.java
@@ -1,7 +1,9 @@
 package com.example.mamiapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,6 +14,7 @@ public class ProductDetailsActivity extends AppCompatActivity {
     private ImageView productImage;
     private TextView productName;
     private TextView productPrice;
+    private ImageView closeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,16 @@ public class ProductDetailsActivity extends AppCompatActivity {
         productImage = findViewById(R.id.pPhoto);
         productName = findViewById(R.id.pName);
         productPrice = findViewById(R.id.pPrice);
+        closeButton = findViewById(R.id.closeButton);
+
+
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ProductDetailsActivity.this, ShopActivity.class);
+                startActivity(i);
+            }
+        });
 
 
         productImage.setImageResource(getIntent().getIntExtra("productImage",0000000));
